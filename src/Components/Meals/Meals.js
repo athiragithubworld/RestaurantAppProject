95f9169bbd2a080a819964f6aa8550d2f3af1908
athiragithubworld <athirahrms@gmail.com>
2023-03-24 19:45:12 +0530

    Meals.js file (Add AvailableMeals with meallist)

--- src/Components/Meals/Meals.js
+++ src/Components/Meals/Meals.js
@@ -2,11 +2,14 @@ import  {Fragment} from "react";
 
 import MealsSummary from "./MealsSummary";
 
+import AvailableMeals from "./AvailableMeals";
+
 
 const Meals = () =>{
     return(
         <Fragment>
         <MealsSummary></MealsSummary>
+        <AvailableMeals></AvailableMeals>
         </Fragment>
     )
 }
