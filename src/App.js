2641ec8bc606a0c1ec47b5ded2d26484dc3e37ad
athiragithubworld <athirahrms@gmail.com>
2023-03-24 10:46:29 +0530

    App.js file (Add summary)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Fragment } from "react";
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 
 
 function App() {
   return (
     <Fragment>
       <Header></Header>
-      
+     <main>
+        <Meals></Meals>
+        </main>
     </Fragment>
   );
 }
