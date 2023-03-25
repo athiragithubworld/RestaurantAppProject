b362b9fccb3fb62a6834df71afd2d63489dffef0
athiragithubworld <athirahrms@gmail.com>
2023-03-25 20:57:48 +0530

    MealItem.js file (Add Mealitem form for adding amount and button)

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,5 +1,5 @@
 import classes from './MealItem.module.css';
- 
+import MealItemForm from './MealItemForm';
 
 const MealItem =(props) =>{
 const price=`$${props.price}`;
@@ -8,11 +8,20 @@ const price=`$${props.price}`;
     return(
 <li className={classes.meal}>
     <div>
-        <h3>{props.name}</h3>
+        <h3>{props.name}
+        
+        </h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
     </div>
-    <div></div>
+    <div>
+    <MealItemForm></MealItemForm>
+    {/* <div><label className={classes.label} >Amount</label>
+    <input type="text"></input>
+    </div>
+    <button style={{display:'right'}}>+ Add</button> */}
+        
+    </div>
 </li>
     )
 }
