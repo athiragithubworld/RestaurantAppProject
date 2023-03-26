cea50c8ebb9e7ffac6e55c676faeec82daf155e9
athiragithubworld <athirahrms@gmail.com>
2023-03-26 12:44:55 +0530

    App.js file ( Add cart file )

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
-
+import CartForm from "./Components/Cart/CartForm";
 
 function App() {
   return (
     <Fragment>
+      <CartForm></CartForm>
       <Header></Header>
      <main>
         <Meals></Meals>
