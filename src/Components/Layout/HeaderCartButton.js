03a8e289ca5e9d90a58003125d395aa1bb96803c
athiragithubworld <athirahrms@gmail.com>
2023-03-26 14:44:31 +0530

    HeaderCartButton.js file (Cart button open the cart)

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,32 +1,32 @@
 import react ,{useState} from "react";
 
 import CartIcon from "../Cart/CartIcon";
-import CartForm from "../Cart/CartForm";
+// import CartForm from "../Cart/CartForm";
 import classes from "./HeaderCartButton.module.css";
 
 
 
 const HeaderCartButton = (props) => {
 
-  const [cartOpen , setCartOpen] = useState(false);
+  // const [cartOpen , setCartOpen] = useState(false);
 
-  const cartClickHandler = () =>{
-    setCartOpen(true)
-  }
+  // const cartClickHandler = () =>{
+  //   setCartOpen(true)
+  // }
 
 
   return (
     <div>
-    {!cartOpen && <button className={classes.button} onClick={cartClickHandler}>
+    { <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon></CartIcon>
       </span>
       <span>Your Cart</span>
       <span className={classes.badge}>0</span>
     </button>}
-    {cartOpen && 
-    <CartForm></CartForm>
-    }
+     
+    {/* <CartForm></CartForm> */}
+    
     </div>
   );
 };
