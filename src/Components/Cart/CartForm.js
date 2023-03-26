49c5a6184aa1b62541ed18705e259623cf74100a
athiragithubworld <athirahrms@gmail.com>
2023-03-26 14:46:43 +0530

    CartForm.js( close button close the cart)

--- src/Components/Cart/CartForm.js
+++ src/Components/Cart/CartForm.js
@@ -1,11 +1,14 @@
+import React from 'react';
+
 import Modal from '../UI/Modal';
 import classes from './CartForm.module.css';
 
-const CartForm =() =>{
+const CartForm =(props) =>{
   // const cartItems ={
    
   // }
 
+ 
     return(
         // <section className={classes.cartForm}>
           
@@ -16,8 +19,10 @@ const CartForm =() =>{
           <span>Total Amount</span>
         </div>
         <div className={classes.actions}>
-        <button className={classes['button--alt']} >Close</button>
+         <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
           <button className={classes.button}>Order</button> 
+
+
           {/* <button className={classes['cart-button']} style={{backgroundColor:'white' ,color:'#8a2b06' }}>Close</button>
           <button className={classes['cart-button']}>Order</button>   */}
         </div>
