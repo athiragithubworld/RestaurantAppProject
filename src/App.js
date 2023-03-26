236986e9e761939ef30b7960f1e9ed6bb5a06d39
athiragithubworld <athirahrms@gmail.com>
2023-03-26 14:38:32 +0530

    App.js file (Cart button -open & close cart )

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { Fragment } from "react";
+import React, { Fragment , useState } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 import CartForm from "./Components/Cart/CartForm";
 
 function App() {
+
+const [openCart ,setOpenCart] = useState(false)
+
+const clickOpenCart = () =>{
+  setOpenCart(true)
+}
+
+const closeCart = () =>{
+  setOpenCart(false)
+}
+
   return (
     <Fragment>
-      <CartForm></CartForm>
-      <Header></Header>
+     {openCart && <CartForm onClose={closeCart}></CartForm>}
+      <Header onClick={clickOpenCart}></Header>
      <main>
         <Meals></Meals>
         </main>
