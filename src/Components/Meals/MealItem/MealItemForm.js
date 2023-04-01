c7585abdce096e58ece52c667f864c75999a15bc
athiragithubworld <athirahrms@gmail.com>
2023-04-01 11:29:53 +0530

    MealItemForm js file ( Created Function --> Add btn -->Get value to cart)

--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -1,19 +1,37 @@
+import react , {useContext} from "react";
+
 import classes from "./MealItemForm.module.css";
 
 import Input from "../../UI/Input";
 
+import CartContext from "../../../store/cart-context";
+
 const MealItemForm = (props) => {
+
+  const cartcxt = useContext(CartContext);
+
+
+  const addItemTocart = (event) =>{
+    event.preventDefault();
+
+  
+    const quantity = document.getElementById('amount_' + props.id).value
+    cartcxt.addItem({...props.item , quantity:quantity})
+
+  }
+
   return (
     <form className={classes.form}>
       <Input label="Amount" input={{
-        id:'amount',
+        id:'amount_' + props.id,
         type:'number',
         min:'1',
         max:'5',
+        step:'1',
         defaultValue :'1'
 
       }}></Input>
-      <button>+ Add</button>
+      <button onClick = {addItemTocart}>+ Add</button>
     </form>
   );
 };
