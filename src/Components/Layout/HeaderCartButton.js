ff1aabf32b912a9f3b36ca7510971d0440bbc3c5
athiragithubworld <athirahrms@gmail.com>
2023-04-01 11:35:20 +0530

    HeaderCartButton js file (Add quanity to cart)

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,12 +1,28 @@
-import react ,{useState} from "react";
+import react, { useContext } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 // import CartForm from "../Cart/CartForm";
 import classes from "./HeaderCartButton.module.css";
 
+import CartContext from "../../store/cart-context";
+
 
 
 const HeaderCartButton = (props) => {
+ 
+  const cartctx = useContext(CartContext);
+
+  let quantity = 0;
+  
+  cartctx.items.forEach(item => {
+    
+    quantity = quantity + parseInt(item.quantity);
+    
+  });
+ 
+  // const numberOfCartItems = cartctx.items.reduce((curNumber ,item) =>{
+  //   return curNumber + item.amount;
+  // } ,0 )
 
   // const [cartOpen , setCartOpen] = useState(false);
 
@@ -14,19 +30,19 @@ const HeaderCartButton = (props) => {
   //   setCartOpen(true)
   // }
 
-
   return (
     <div>
-    { <button className={classes.button} onClick={props.onClick}>
-      <span className={classes.icon}>
-        <CartIcon></CartIcon>
-      </span>
-      <span>Your Cart</span>
-      <span className={classes.badge}>0</span>
-    </button>}
-     
-    {/* <CartForm></CartForm> */}
-    
+      {
+        <button className={classes.button} onClick={props.onClick}>
+          <span className={classes.icon}>
+            <CartIcon></CartIcon>
+          </span>
+          <span>Your Cart</span>
+          <span className={classes.badge}>{quantity}</span>
+        </button>
+      }
+
+      {/* <CartForm></CartForm> */}
     </div>
   );
 };
